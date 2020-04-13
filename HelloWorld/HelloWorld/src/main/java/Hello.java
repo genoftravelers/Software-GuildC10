0208202f44ffaae3934b30863b21fc419ffa118a
yooramachi <krmdlkr@gmail.com>
2020-04-13 16:30:47 -0400

    Hello World in NetBeans

---
+++ HelloWorld/HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Karma Dolkar
+ */
+package com.KD.helloworld; // Use YOUR Group Id value here instead of com.tsg
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Karma Dolkar");
+    }
+}
