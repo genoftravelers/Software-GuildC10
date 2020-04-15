f871bf4a262f0b618cb9892b1023a7e0d24e0d6f
yooramachi <krmdlkr@gmail.com>
2020-04-15 12:16:34 -0400

    Lesson 4 Complete.

---
+++ SoftwareGuildC10/src/main/java/PracticeProgramThree/BiggerBetterAdder.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PracticeProgramThree;
+
+import java.util.*;
+
+/**
+ *
+ * @author yooramachi
+ */
+public class BiggerBetterAdder {
+    public static void main(String[] args) {
+        
+        
+        Scanner SC = new Scanner(System.in);
+        
+        System.out.println("Give me the first number.");
+        int num1 = Integer.parseInt(SC.nextLine());
+       
+        System.out.println("Give me the second number.");
+        int num2 = Integer.parseInt(SC.nextLine());
+
+        System.out.println("Give me your third number.");
+        int num3 = Integer.parseInt(SC.nextLine());
+        
+        int newNum = num1 + num2 + num3;
+        System.out.println("Result: " + newNum);
+    }
+}
---
+++ SoftwareGuildC10/src/main/java/PracticeProgramThree/DoItBetter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PracticeProgramThree;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author yooramachi
+ */
+public class DoItBetter {
+    public static void main(String[] args) {
+        int milesRun;
+        int numOfHotDogs;
+        int numOfLangs;
+        
+        Scanner SC = new Scanner(System.in);
+        
+        System.out.println("Hello there, how many miles can you run?");
+        milesRun = Integer.parseInt(SC.nextLine());
+        System.out.println("thats it? I can run " + ((milesRun * 2) + 1) + " miles haha!");
+        System.out.println("How many hotdogs can you eat? ");
+        numOfHotDogs = Integer.parseInt(SC.nextLine());
+        System.out.println("thats it? I can eat "+ ((numOfHotDogs*2)+1) + " Hot Dogs.");
+        System.out.println("how many languages can you speak?");
+        numOfLangs = Integer.parseInt(SC.nextLine());
+        System.out.println("too bad I can speak "+ ((numOfLangs *2)+1) +" languages");
+    }
+}
