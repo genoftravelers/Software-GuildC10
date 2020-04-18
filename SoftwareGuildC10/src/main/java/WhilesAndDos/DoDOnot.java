08ae425c68ba4367146d43088bbb6bdc50b6ffac
yooramachi <krmdlkr@gmail.com>
2020-04-18 16:18:37 -0400

    While Dos Complete.

---
+++ SoftwareGuildC10/src/main/java/WhilesAndDos/DoDOnot.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WhilesAndDos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Karma Dolkar <krmdlkr@gmail.com>
+ */
+public class DoDOnot {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Should I do it? (y/n) ");
+        boolean doIt;
+
+        if (input.next().equals("y")) {
+            doIt = true; // DO IT!
+        } else {
+            doIt = false; // DONT YOU DARE!
+        }
+
+        boolean iDidIt = false;
+
+//        do {
+//            iDidIt = true;
+//            break;
+//        } while (doIt);
+         while(doIt){
+             iDidIt = true;
+             break;
+         }
+
+        if (doIt && iDidIt) {
+            System.out.println("I did it!");
+        } else if (!doIt && iDidIt) {
+            System.out.println("I know you said not to ... but I totally did anyways.");
+        } else {
+            System.out.println("Don't look at me, I didn't do anything!");
+    }
+}
+}
+
+/*
+- It prints "I did it".
+- It prints "I did it anyways."
+-prints below
+
+Should I do it? (y/n) n
+Don't look at me, I didn't do anything!
+
+Should I do it? (y/n) y
+I did it!
+*/
---
+++ SoftwareGuildC10/src/main/java/WhilesAndDos/StayPositive.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WhilesAndDos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Karma Dolkar <krmdlkr@gmail.com>
+ */
+public class StayPositive {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("What number should I count down from? ");
+        
+        int userInput = Integer.parseInt(sc.nextLine());
+        
+        if(userInput > 0){
+           System.out.println("It is a positive integer.");
+            System.out.println("Here goes!");
+       } else if (userInput < 0 ) {
+           System.out.println(" Sorry, I dont do negative integers");
+       }
+       
+       // int startNum = 10;
+        int finishNum = 1;
+        
+                
+        while (userInput >= finishNum) {
+            userInput--;
+            System.out.println(userInput);
+        }
+       
+    }
+}
